939ef6539261e36d611219021e029a65d529c470
maartenverheul <maarten.verheul@student.hu.nl>
2021-01-08 10:59:27 +0100

    Update notepad

--- src/App.tsx
+++ src/App.tsx
@@ -9,16 +9,15 @@ function App() {
       <div className="desktop screen show">
         <div className="app notepad focus" style={{left: 100, top: 100}}>
           <div className="app-header">
-            <div className="app-icon">
-
-            </div>
-            <span className="app-title">*Untitled - Notepad</span>
+            <div className="app-icon"></div>
+            <span className="app-title">Maarten Verheul.txt - Notepad</span>
           </div>
           <div className="app-content">
-            Maarten Verheul.
+            Welcome to the website of Maarten Verheul.
             <br/>
             <br/>
-            <a target="blank" href="https://www.linkedin.com/in/maarten-verheul/">LinkedIn</a>
+            <a target="blank" href="https://www.linkedin.com/in/maarten-verheul/">LinkedIn</a> -&nbsp;
+            <a target="blank" href="https://www.instagram.com/meerofmindermaarten/">Instagram</a>
           </div>
         </div>
       </div>

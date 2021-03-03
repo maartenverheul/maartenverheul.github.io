19668ad7610851bf23528e888d28577f21ae8844
maartenverheul <maarten.verheul@student.hu.nl>
2021-03-03 23:18:16 +0100

    Add appshell transitions

--- src/components/appshell/AppShell.tsx
+++ src/components/appshell/AppShell.tsx
@@ -15,20 +15,31 @@ type State = {
   isMoving: boolean,
   moveOffset: {x: number, y: number}|null,
   moverLocation: {x: number, y: number}|null,
-  show: boolean
+  show: boolean,
+  state: AppShellState,
+  easeTime: number
+}
+
+enum AppShellState {
+  NORMAL = "normal",
+  MAXIMIZED = "maximized",
+  MINIMIZED = "minimized",
+  DESTROYED = "destroyed",
 }
 
 class AppShell extends React.Component<Props> {
 
   private static readonly MOVERSIZE: [number, number] = [400, 400]
 
-  public state: State = {
+  state: State = {
     position: {x: this.props.position[0], y: this.props.position[1]},
     size: {width: this.props.size[0], height: this.props.size[1]},
     isMoving: false,
     moveOffset: null,
     moverLocation: null,
-    show: true
+    show: true,
+    state: AppShellState.NORMAL,
+    easeTime: 200,
   }
   private element: React.RefObject<HTMLDivElement>;
 
@@ -40,6 +51,7 @@ class AppShell extends React.Component<Props> {
   private onPointerDown(e: React.PointerEvent<HTMLDivElement>){
     if(!this.element.current || (e.target as HTMLDivElement).classList.contains("app-header-button")) return; //  If down on buttons, return.
     this.setState({
+      easeTime: 0,
       isMoving: true,
       moveOffset: { x: e.clientX - this.element.current.offsetLeft, y: e.clientY - this.element.current.offsetTop },
       moverLocation: { x: e.clientX - this.element.current.offsetLeft - AppShell.MOVERSIZE[0]/2, y: e.clientY - this.element.current.offsetTop - AppShell.MOVERSIZE[0]/2 },
@@ -62,32 +74,37 @@ class AppShell extends React.Component<Props> {
     });
   }
   private close(){
-    this.destroy();
+    // this.destroy();
+    this.setState({ easeTime: 100, state: AppShellState.DESTROYED });
   }
-  private minimize(){
+  private toggleMinimize(){
+    this.setState({ easeTime: 300, state: AppShellState.MINIMIZED });
+  }
+  private toggleMaximize(){
+    let newState = this.state.state == AppShellState.NORMAL ? AppShellState.MAXIMIZED : AppShellState.NORMAL;
+    this.setState({ easeTime: 80, state: newState });
+  }
 
-  }
-  private maximize(){
-    
-  }
-
-  public resize(width: number, height: number): void {
+  resize(width: number, height: number): void {
     this.setState({
       size: {
         width, height
       }
     })
   }
-  public destroy(): void {
+  destroy(): void {
     this.setState({
       show: false
     })
   }
 
-  public render() {
-    if(!this.state.show) return <p></p>;
+  render() {
+    if(!this.state.show) return <div></div>;
+
+    const appTransition = `all ${this.state.easeTime}ms ease-in-out`; 
+    
     return (
-      <div className="app focus" style={{left: this.state.position.x, top: this.state.position.y, width: this.state.size.width, height: this.state.size.height}} ref={this.element}>
+      <div className={`app app-state-${this.state.state} focus`} style={{transition: appTransition, left: this.state.position.x, top: this.state.position.y, width: this.state.size.width, height: this.state.size.height}} ref={this.element}>
         {this.state.isMoving && <div className="app-mover" style={{left: this.state.moverLocation?.x, top: this.state.moverLocation?.y, width: AppShell.MOVERSIZE[0], height: AppShell.MOVERSIZE[1]}} onPointerMove={(e) => this.onPointerMove(e)} onPointerUp={() => this.onPointerUp()}></div>}
         <div className="app-header" onPointerDown={(e) => this.onPointerDown(e)}>
           
@@ -96,8 +113,8 @@ class AppShell extends React.Component<Props> {
           
           <div className="right">
             <button className="app-header-button red" onMouseUp={() => this.close()}>X</button>
-            <button className="app-header-button" onMouseUp={() => this.maximize()}>[]</button>
-            <button className="app-header-button" onMouseUp={() => this.minimize()}>-</button>
+            <button className="app-header-button" onMouseUp={() => this.toggleMaximize()}>[]</button>
+            <button className="app-header-button" onMouseUp={() => this.toggleMinimize()}>-</button>
           </div>
         </div>
         <div className="app-content">

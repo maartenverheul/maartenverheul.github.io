62026d65a864179cd16c8a3cdee21ae4d411b26e
maartenverheul <maarten.verheul@student.hu.nl>
2021-03-05 22:52:50 +0100

    Clean Notepad

--- src/apps/NotepadApp.tsx
+++ src/apps/NotepadApp.tsx
@@ -1,37 +1,18 @@
 import React from "react";
 import AppShell from "../components/appshell/AppShell";
 
-const url = `https://raw.githubusercontent.com/maartenverheul/maartenverheul.github.io/master/content/CV%20Maarten%20Verheul%20-%20nederlands.txt`
+type Props = {
+  filename?: string
+  content: string
+}
 
-export default class NotepadApp extends React.Component {
-
-  state = {
-    content: ""
-  }
-
-  componentDidMount(){
-    this.getData();
-  }
-
-  async getData(){
-    
-    let data = await fetch(url)
-    .then(data => {
-      return data.text();
-    });
-    this.setState({content: data});
-  }
+export default class NotepadApp extends React.Component<Props> {
 
   render(){
     return (
-      <AppShell title="Maarten Verheul.txt - Notepad" icon="icon" position={[100, 100]} size={[800, 600]}>
-        <div style={{overflow: "auto", height: "100%", marginLeft: 3, paddingBottom: 10, fontFamily: 'consolas'}}>
-          {/* <p style={{whiteSpace: "pre"}}>{this.state.content}</p> */}
-          <p>Welcome to the website of Maarten Verheul.<br/>
-          <a target="blank" href="https://www.linkedin.com/in/maarten-verheul/">LinkedIn</a> -&nbsp;
-          <a target="blank" href="https://www.instagram.com/meerofmindermaarten/">Instagram</a>
-          </p><br/>
-          <div className="img-coffee"></div>
+      <AppShell title={`${this.props.filename||"Untitled"} - Notepad`} icon="icon" position={[100, 100]} size={[800, 600]}>
+        <div style={{overflowY: "scroll", height: "100%", marginLeft: 3, paddingBottom: 10, fontFamily: 'consolas'}}>
+          <p style={{whiteSpace: "pre"}}>{this.props.content}</p>
         </div>
       </AppShell>
     )
--- src/pages/Desktop.tsx
+++ src/pages/Desktop.tsx
@@ -3,14 +3,32 @@ import './Desktop.scss';
 import ShortcutLayer from '../components/shortcutlayer/ShortcutLayer';
 import Taskbar from '../components/taskbar/Taskbar';
 import NotepadApp from '../apps/NotepadApp';
+import Content from '../config/content.json';
 
 class Desktop extends React.Component {
+
+  state = {
+    content: ""
+  }  
+
+  componentDidMount(){
+    this.getData();
+  }
+
+  async getData(){
+    
+    let data = await fetch(Content.MaartenVerheul)
+    .then(data => {
+      return data.text();
+    });
+    this.setState({content: data});
+  }
 
   render(){
     return (
       <div className="desktop screen show">
         <ShortcutLayer/>
-        <NotepadApp />
+        <NotepadApp filename="Maarten Verheul.txt" content={this.state.content} />
         <Taskbar />
       </div>
     )
